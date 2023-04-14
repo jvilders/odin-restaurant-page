6624aacdbc4be43217e4693076e3357747bc5f86
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-04-14 18:28:57 +0200

    First commit
    Used my typescript template, added webpack and set up the dev server.

---
+++ webpack.config.cjs
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({title: "Restaurant page"})
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+};
