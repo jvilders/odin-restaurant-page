795f687b96503e58de8c882581fe300e5ac7b689
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-04-16 15:38:35 +0200

    Add Menu in html
    Used ChatGPT for items on the menu. Unintentionally the layout is
    pretty responsive already. A media query (or some auto-fit stuff)
    to change the entire menu grid over to a 1 column layout could improve
    it more.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 import { buildHomePage } from "./pages/index.js";
 
-const homepageMain = buildHomePage();
-document.querySelector(".content")?.appendChild(homepageMain);
-console.log("added homepage content");
+// const homepageMain = buildHomePage();
+// document.querySelector(".content")?.appendChild(homepageMain);
+// console.log("added homepage content");
