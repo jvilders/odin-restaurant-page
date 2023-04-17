a82f2c4eab9b276360382f77a0187095768af754
Jona Vilders <60934195+jvilders@users.noreply.github.com>
2023-04-17 21:53:21 +0200

    Start on the contact page
    Add staff cards, add opening hours

--- src/index.ts
+++ src/index.ts
@@ -8,44 +8,44 @@ function verifyNotNull<T>(p: null | T): T {
   return p;
 }
 
-const contentAttachPoint = verifyNotNull(document.querySelector(".content"));
-const homepageMain = buildHomePage();
-let currentPage = "homepage";
-contentAttachPoint.appendChild(homepageMain);
-console.log("added homepage content");
+// const contentAttachPoint = verifyNotNull(document.querySelector(".content"));
+// const homepageMain = buildHomePage();
+// let currentPage = "homepage";
+// contentAttachPoint.appendChild(homepageMain);
+// console.log("added homepage content");
 
-const homeNavigation = verifyNotNull(
-  document.querySelector(".page-buttons .home")
-);
-const menuNavigation = verifyNotNull(
-  document.querySelector(".page-buttons .menu")
-);
-const contactNavigation = verifyNotNull(
-  document.querySelector(".page-buttons .contact")
-);
+// const homeNavigation = verifyNotNull(
+//   document.querySelector(".page-buttons .home")
+// );
+// const menuNavigation = verifyNotNull(
+//   document.querySelector(".page-buttons .menu")
+// );
+// const contactNavigation = verifyNotNull(
+//   document.querySelector(".page-buttons .contact")
+// );
 
-homeNavigation.addEventListener("click", () => {
-  if (currentPage == "homepage") {
-    return;
-  }
-  contentAttachPoint.innerHTML = "";
-  contentAttachPoint.appendChild(buildHomePage());
-  currentPage = "homepage";
-});
+// homeNavigation.addEventListener("click", () => {
+//   if (currentPage == "homepage") {
+//     return;
+//   }
+//   contentAttachPoint.innerHTML = "";
+//   contentAttachPoint.appendChild(buildHomePage());
+//   currentPage = "homepage";
+// });
 
-menuNavigation.addEventListener("click", () => {
-  if (currentPage == "menu") {
-    return;
-  }
-  contentAttachPoint.innerHTML = "";
-  contentAttachPoint.appendChild(buildMenuPage());
-  currentPage = "menu";
-});
+// menuNavigation.addEventListener("click", () => {
+//   if (currentPage == "menu") {
+//     return;
+//   }
+//   contentAttachPoint.innerHTML = "";
+//   contentAttachPoint.appendChild(buildMenuPage());
+//   currentPage = "menu";
+// });
 
-contactNavigation.addEventListener("click", () => {
-  if (currentPage == "contact") {
-    return;
-  }
-  console.log("Unimplemented for now :(");
-  currentPage = "contact";
-});
+// contactNavigation.addEventListener("click", () => {
+//   if (currentPage == "contact") {
+//     return;
+//   }
+//   console.log("Unimplemented for now :(");
+//   currentPage = "contact";
+// });
